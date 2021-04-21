52860948ddbbc9cbc42095a23ed87184ecfd83ba
A.E <a.e@AEs-MacBook-Air.local>
2021-04-21 14:23:02 +0200

    Updates

---
+++ precache-manifest.dc546ffa2304b219f7544ba037519048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b407755cf324f282e34556b74971b2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "588d60348d3376a8146c",
+    "url": "./static/js/2.c78a912f.chunk.js"
+  },
+  {
+    "revision": "8bdcc729fc0aa4b1acd60ad711e551ed",
+    "url": "./static/js/2.c78a912f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e746e60b7ddd938df50",
+    "url": "./static/js/main.0aab66c0.chunk.js"
+  },
+  {
+    "revision": "f56fcf29d8f08a123835",
+    "url": "./static/js/runtime-main.726dc72a.js"
+  }
+]);
